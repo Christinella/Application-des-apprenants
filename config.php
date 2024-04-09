5043f38d382491616eb46dcb22a777c7d88a5b50
herivola <rchristinella@gmail.com>
2024-04-09 11:34:27 +0200

    ajout dans le fichier htaccess, config, init, index et creer le fichier includes pour le header

---
+++ config.php
@@ -0,0 +1,10 @@
+<?php
+
+define('DB_HOST', 'application');
+define('DB_NAME', 'application');
+define('DB_USER', 'application');
+define('DB_PWD', 'application');
+
+define('HOME_URL', '/');
+
+define('DB_INITIALIZED', TRUE);
